77956183438f03885bf75add0b1636df0441249c
Nuno Sousa <9180579@esmad.ipp.pt>
2021-03-03 19:22:47 +0000

    Project started - Navbar in progress

---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="header flex flex-jc-sb flex-ai-c">
+    <h2 class="header__title">Moitas Car</h2>
+    <div class="header__hamburger" @click="toggleNavbar">
+      <div class="header__hamburger__lines"></div>
+      <div class="header__hamburger__lines"></div>
+      <div class="header__hamburger__lines"></div>
+    </div>
+    <div class="header__mobile__menu">
+      <div class="header__mobile__menu__links">
+        <h3><span>01</span> Homepage</h3>
+        <h3><span>02</span> Carros</h3>
+        <h3><span>03</span> Sobre Nós</h3>
+        <h3><span>04</span> Contactos</h3>
+        <h3><span>05</span> Fale Connosco</h3>
+      </div>
+      <div class="header__mobile__menu__socials">
+        <h3>Facebook</h3>
+        <h3>WhatsApp</h3>
+      </div>
+      <div class="header__mobile__menu__policy">
+        <p>Privacy Policy & © Copy Right 2021</p>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  methods: {
+    toggleNavbar() {
+      let lines = document.querySelectorAll(".header__hamburger__lines");
+
+      lines.forEach(line => {
+        line.classList.toggle("open");
+      });
+
+      let title = document.querySelector(".header__title");
+      title.classList.toggle("change__color");
+
+      let header__mobile__menu = document.querySelector(
+        ".header__mobile__menu"
+      );
+
+      header__mobile__menu.classList.toggle("open__overlay");
+    }
+  }
+};
+</script>
+
+<style></style>
